3023afad98e96ee79c90742cf1bad2ca2f0965c9
Your Name <deeshitn.tt@gmail.com>
2026-03-03 13:56:04 +0530

    new sttt

--- db/ConnectionDB.js
+++ db/ConnectionDB.js
@@ -1,19 +1,14 @@
 // db/ConnectionDB.js
 const mongoose = require('mongoose');
 
-let cached = global.mongoose;
-if (!cached) cached = global.mongoose = { conn: null, promise: null };
-
 const MongoDBConnect = async () => {
-  if (cached.conn) return cached.conn;
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then(m => m);
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
+   mongoose.connect(process.env.MONGO_URI)
+   .then(()=>{
+    console.log('MongoDB connect sucess')
+   })
+   .catch((err)=>{
+    console.log(err)
+   })
 };
 
 module.exports = MongoDBConnect;
